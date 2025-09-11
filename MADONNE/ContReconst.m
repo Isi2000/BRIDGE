b6ea1e44ea8a81fd13134880f4b56022a3c3d876
Isacco <isaccofaglioni2000@gmail.com>
2025-09-11 17:51:26 +0200

    .

--- MADONNE/ContReconst.m
+++
@@ -1,9 +0,0 @@
-function Reconst=ContReconst(t,t0,u,Time,GrowthRate,Frequency)
-
-[N,M]=size(u);
-vv=zeros(M,1);
-for m=1:M
- vv(m)=exp((GrowthRate(m)+i*Frequency(m))*(t-t0));   
-end
-
-Reconst=u*vv;
